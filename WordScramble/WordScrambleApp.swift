a8944e88c98e6ed87fa0b2f74ae2bff9fa30f246
naariman <pticafugu@gmail.com>
2024-08-07 13:40:45 +0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nariman Nogaibayev on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
